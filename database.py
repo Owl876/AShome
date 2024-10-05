478c4b40db9b14c713fe98cc4265d45fb01a2dc5
Owl876 <dobretskovda@gmail.com>
2024-10-05 16:02:47 +0300

    +changes db

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://Owl:8679@localhost:5432/site"  # Замените на свой пароль
+DATABASE_URL = "postgresql://Owl:8679@localhost/site"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
