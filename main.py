9e096ee7c4abbef0847da883c2b875681479c120
Owl876 <dobretskovda@gmail.com>
2024-10-05 17:43:43 +0300

    +messenger

--- main.py
+++ main.py
@@ -75,21 +75,6 @@ def login(request: Request, username: str = Form(...), password: str = Form(...)
 
     raise HTTPException(status_code=400, detail="Неверные имя пользователя или пароль")
 
-"""
-@app.post("/send/")
-def send_message(request: Request, recipient_id: int = Form(...), content: str = Form(...), db: Session = Depends(get_db), user_id: int = Depends(get_current_user_id)):
-    message = models.Message(sender_id=user_id, recipient_id=recipient_id, message_content=content)
-    db.add(message)
-    db.commit()
-    db.refresh(message)
-    return templates.TemplateResponse("messenger.html", {"request": request, "message": "Сообщение отправлено!"})
-
-
-@app.get("/messenger/", response_class=HTMLResponse)
-def get_messenger(request: Request, db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return templates.TemplateResponse("messenger.html", {"request": request, "users": users})
-"""
 # Зависимость для получения сессии базы данных
 def get_db():
     db = SessionLocal()
@@ -124,12 +109,17 @@ def get_messenger(request: Request, db: Session = Depends(get_db)):
 
 # Отправка сообщения
 @app.post("/send/")
-def send_message(recipient_id: int = Form(...), content: str = Form(...), db: Session = Depends(get_db), user: models.User = Depends(get_current_user)):
-    message = models.Message(sender_id=user.id, recipient_id=recipient_id, message_content=content)
+def send_message(request: Request, recipient_id: int = Form(...), content: str = Form(...), db: Session = Depends(get_db), user_id: int = Depends(get_current_user_id)):
+    if not recipient_id or not content:
+        raise HTTPException(status_code=400, detail="Recipient ID and message content are required")
+
+    message = models.Message(sender_id=user_id, recipient_id=recipient_id, message_content=content, timestamp=str(datetime.datetime.now()))
     db.add(message)
     db.commit()
     db.refresh(message)
-    return {"message": "Сообщение отправлено!"}
+
+    return {"message": message.message_content}
+
 
 # Получение сообщений для чата
 @app.get("/messages/{recipient_id}", response_class=JSONResponse)
