0e91381b414bdab5504c2b729249502c0ea9c388
Owl876 <dobretskovda@gmail.com>
2024-10-05 17:07:55 +0300

    +messenger

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 # models.py
-
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -11,16 +10,17 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
 
-    messages = relationship("Message", back_populates="user")
+    # Определяем связь с сообщениями
+    messages_sent = relationship("Message", foreign_keys="Message.sender_id", back_populates="sender")
+    messages_received = relationship("Message", foreign_keys="Message.recipient_id", back_populates="recipient")
 
 class Message(Base):
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True, index=True)
-    sender_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # Внешний ключ на пользователя
-    recipient_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # Внешний ключ на пользователя
-    message_content = Column(String, nullable=False)
-    timestamp = Column(String, nullable=False)
+    sender_id = Column(Integer, ForeignKey('users.id'))
+    recipient_id = Column(Integer, ForeignKey('users.id'))
+    message_content = Column(String)
 
-    sender = relationship("User", foreign_keys=[sender_id], backref="sent_messages")
-    recipient = relationship("User", foreign_keys=[recipient_id], backref="received_messages")
+    sender = relationship("User", foreign_keys=[sender_id], back_populates="messages_sent")
+    recipient = relationship("User", foreign_keys=[recipient_id], back_populates="messages_received")
